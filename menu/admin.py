893085f481f910c1f97744f500b1e3d00d1ff054
Denise l Antonio <127862370+deeantoh@users.noreply.github.com>
2024-06-10 12:44:47 +0200

    Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -28,7 +28,7 @@ class BorrowingAdmin(admin.ModelAdmin):
    
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title","author","date_added")
+    list_display = ("title","author","date_published")
     search_fields = ("title","author",)
 
 
